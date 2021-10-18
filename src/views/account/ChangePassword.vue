b06827548f7b622583b309eca54fd65864774091
Srdjan Kordic <srdjan@hejdev.com>
2021-10-18 12:22:18 +0200

    Added change password functionality for account

--- src/views/account/ChangePassword.vue
+++ src/views/account/ChangePassword.vue
@@ -8,40 +8,62 @@
 			</div>
 		</div>
 		<div class="card-body pt-1">
-			<div class="row mb-2">
-				<div class="form-group col-6">
-					<label for="oldPassword" class="form-label">Old Password</label>
-					<input type="password" class="form-control" v-model="password.old" placeholder="Old Password" />
+			<form @submit.prevent="changePassword">
+				<div class="row mb-2">
+					<div class="form-group col-6">
+						<label for="currentPassword" class="form-label">Current Password</label>
+						<input
+							type="password"
+							class="form-control"
+							v-model="current_password"
+							placeholder="Current Password"
+							id="currentPassword"
+							required
+						/>
+					</div>
 				</div>
-			</div>
-			<div class="row mb-2">
-				<div class="form-group col-6">
-					<label for="password" class="form-label">New Password</label>
-					<input type="password" class="form-control" v-model="password.new" placeholder="New Password" />
+				<div class="row mb-2">
+					<div class="form-group col-6">
+						<label for="newPassword" class="form-label">New Password</label>
+						<input
+							type="password"
+							class="form-control"
+							v-model="new_password"
+							placeholder="New Password"
+							id="newPassword"
+							required
+						/>
+					</div>
+					<div class="form-group col-6">
+						<label for="confirmNewPassword" class="form-label">Retype new Password</label>
+						<input
+							type="password"
+							class="form-control"
+							v-model="new_confirm_password"
+							placeholder="New Password"
+							id="confirmNewPassword"
+							required
+						/>
+					</div>
 				</div>
-				<div class="form-group col-6">
-					<label for="confirmPassword" class="form-label">Retype new Password</label>
-					<input type="password" class="form-control" v-model="password.confirm" placeholder="New Password" />
-				</div>
-			</div>
+				<button class="btn btn-primary float-end mt-2">Update</button>
+			</form>
 		</div>
 	</div>
 	<!-- #Change Password -->
 </template>
 
 <script>
-//import restApi from "../api/index.js";
+import restApi from '../../api'
 import { mapState } from 'vuex'
 export default {
 	name: 'ChangePassword',
 	data() {
 		return {
 			user: {},
-			password: {
-				old: '',
-				new: '',
-				confirm: '',
-			},
+			current_password: '',
+			new_password: '',
+			new_confirm_password: '',
 			loading: false,
 		}
 	},
@@ -51,6 +73,56 @@ export default {
 	created() {
 		this.user = this.authUser
 	},
-	methods: {},
+	methods: {
+		changePassword() {
+			this.loading = true
+			let data = {
+				current_password: this.current_password,
+				new_password: this.new_password,
+				new_confirm_password: this.new_confirm_password,
+			}
+			restApi
+				.post('/user/change-password', data)
+				.then(() => {
+					this.loading = false
+					this.$swal.fire({
+						icon: 'success',
+						title: 'Password successfully changed!',
+					})
+				})
+				.catch(error => {
+					let errorMsg = this.handleValidationErrors(error)
+					if (errorMsg != '') {
+						this.$swal.fire({
+							icon: 'error',
+							title: errorMsg,
+							timer: 6000,
+						})
+					} else {
+						this.$swal.fire({
+							icon: 'error',
+							title: error,
+							timer: 6000,
+						})
+					}
+				})
+		},
+		handleValidationErrors(error) {
+			let errorMessage = ''
+			if (error.response.data.errors) {
+				let validationErrors = error.response.data.errors
+				if (validationErrors.current_password) {
+					errorMessage += ' ' + validationErrors.current_password
+				}
+				if (validationErrors.new_confirm_password) {
+					errorMessage += ' ' + validationErrors.new_confirm_password
+				}
+				if (validationErrors.new_password) {
+					errorMessage += ' ' + validationErrors.new_password
+				}
+			}
+			return errorMessage
+		},
+	},
 }
 </script>
